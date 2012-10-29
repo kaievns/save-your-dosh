c1c80606d2f288ebd1de56db1a055e75167bc682
Nikolay Nemshilov <nemshilov@gmail.com>
2012-10-29 18:20:50 +0700

    Stubbing Kernel::

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,10 @@
 require 'rspec'
 
 require File.dirname(__FILE__) + "/../lib/save_your_dosh.rb"
+
+# mocking the Kernel::` calls in the new-relic API proxy
+class SaveYourDosh::NewRelic
+  def self.`(str)
+    ''
+  end
+end
