749813b1abd835cb16a347afe8bce1c855902d7b
Nikolay Nemshilov <nemshilov@gmail.com>
2012-10-29 18:40:37 +0700

    adding the new-relic application busyness calls api

--- lib/save_your_dosh/new_relic.rb
+++ lib/save_your_dosh/new_relic.rb
@@ -9,6 +9,15 @@ class SaveYourDosh::NewRelic
       -H "X-Api-Key: %{api_key}" 2> /dev/null
   }.strip
 
+  THRESHOLD_CMD = %Q{
+    curl https://api.newrelic.com/accounts/%{acc_id}/applications/%{app_id}/threshold_values.xml \
+      -H "X-Api-Key: %{api_key}" 2> /dev/null
+  }.strip
+
+  #
+  # Tries to figure the actual app-id in case if the
+  # user specified the app-name instead
+  #
   def self.get_app_id(config)
     app_id = (config.new_relic['app_id'] || '').to_s
 
@@ -33,6 +42,29 @@ class SaveYourDosh::NewRelic
     end
 
     app_id
+  end
+
+  #
+  # Tries to check the application busyness in percents
+  #
+  # NOTE: returns `0` if the request have failed
+  #
+  def self.get_application_busyness
+    conf = SaveYourDosh.config.new_relic
+
+    data = THRESHOLD_CMD % {
+      acc_id:  conf['acc_id'],
+      app_id:  conf['app_id'],
+      api_key: conf['api_key']
+    }
+
+    data = Nokogiri.parse(`#{data}`)
+
+    data.css('threshold_value[name="Application Busy"]').each do |entry|
+      return entry['metric_value'].to_f
+    end
+
+    nil # fallback
   end
 
 end
