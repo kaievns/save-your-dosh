8e816074420370df8915db58097458437ddaf29e
Nikolay Nemshilov <nemshilov@gmail.com>
2012-10-29 17:30:09 +0700

    Making the config to read the ENV hash for the new-relic configs

--- lib/save_your_dosh/config.rb
+++ lib/save_your_dosh/config.rb
@@ -5,12 +5,16 @@ require 'yaml'
 
 class SaveYourDosh::Config
   DEFAULTS = File.dirname(__FILE__) + "/../save_your_dosh.yml"
-  KEYS     = %w{ new_relic dynos workers notify }
+  KEYS     = %w{ new_relic dynos workers notify interval }
 
   KEYS.each{ |key| attr_accessor key }
 
   def initialize
     read DEFAULTS
+
+    @new_relic['acc_id']  = ENV['NEW_RELIC_ID']
+    @new_relic['app_id']  = ENV['NEW_RELIC_APP_ID']
+    @new_relic['api_key'] = ENV['NEW_RELIC_API_KEY']
   end
 
   def read(file)
--- spec/lib/save_your_dosh/config_spec.rb
+++ spec/lib/save_your_dosh/config_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe SaveYourDosh::Config do
   describe "defaults" do
     before do
+      ENV['NEW_RELIC_ID']      = 'acc-id'
+      ENV['NEW_RELIC_APP_ID']  = 'app-id'
+      ENV['NEW_RELIC_API_KEY'] = 'api-key'
+
+
       @config = SaveYourDosh::Config.new
     end
 
@@ -15,6 +20,14 @@ describe SaveYourDosh::Config do
     it "should have default workders data" do
       @config.workers.should == {
         "min" => 1, "max" => 5, "jobs" => 20
+      }
+    end
+
+    it "should read the new_relic settings from the ENV hash" do
+      @config.new_relic.should == {
+        'acc_id'  => ENV['NEW_RELIC_ID'],
+        'app_id'  => ENV['NEW_RELIC_APP_ID'],
+        'api_key' => ENV['NEW_RELIC_API_KEY']
       }
     end
   end
